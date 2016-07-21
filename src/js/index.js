71e70b84d9034cdfe324e832a1e988c5e4a42d39
tml <2385695960@qq.com>
2016-07-21 15:59:36 +0800

    提交

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,15 @@
 $(function(){
+	//topbar中有子菜单的li进行遍历
 	$('.submenue').each(function(idx,ele){
 		$(ele).hover(function(){
 			$(this).find('div').show();
-			$(this).find('a').eq(0).addClass('show_submenue');
+			$(this).addClass('show_submenue');
 		},function(){
 			$(this).find('div').hide();
-			$(this).find('a').eq(0).removeClass('show_submenue');
+			$(this).removeClass('show_submenue');
 		});
 	});
+	//侧边导航的li会出现子菜单，li中样式会改变
 	$('.left_nav li').each(function(idx,ele){
 		$(ele).hover(function(){
 			$(this).addClass('left_nav_hover');
@@ -17,6 +19,14 @@ $(function(){
 			$(this).removeClass('left_nav_hover');
 			$(this).find('a').css('color','#fff')
 			$(this).find('div').hide();
+		});
+	});
+	//精品团购中的li在hover的时候样式变化
+	$('.jtuan_list li').each(function(idx,ele){
+		$(ele).hover(function(){
+			$(this).find('.border').show();
+		},function(){
+			$(this).find('.border').hide();
 		});
 	});
 });
